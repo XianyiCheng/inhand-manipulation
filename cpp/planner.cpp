a1cc284190fb63120a5656bdb483ef2cb73af2d9
Ian Char <ianchar4@gmail.com>
2020-11-18 00:23:09 -0500

    Added RRT Connect

--- cpp/planner.cpp
+++ cpp/planner.cpp
@@ -16,12 +16,23 @@
 
 /* Planner Ids */
 #define RRT         0
+#define RRTCONNECT  1
 
 /* Output Arguments */
 #define	OBJECTPATH_OUT	plhs[0]
 #define	FINGERPATH_OUT	plhs[1]
 #define	PLANLENGTH_OUT	plhs[2]
 #define	TREESIZE_OUT	plhs[3]
+
+// tunable parameters
+double goal_thr = PI*1/180;
+double goal_biased_prob = 0.7;
+double primitive1_prob = 0.5;
+double epsilon_translation = 1;
+double epsilon_angle = PI*45/180;
+int interpolation_steps = 10;
+double interpolation_length = 1/double(interpolation_steps);
+int max_samples = 1000;
 
 void to_config(double config[7], Vector3d p, Quaterniond q){
     config[0] = p[0];
@@ -95,6 +106,63 @@ double epsilon_translation, double epsilon_angle, int steps, Tree* T, double fin
 
 }
 
+int connect(int* near_idx, double config[7], double epsilon_translation,
+            double epsilon_angle, int steps, Tree* T,
+            double finger_workspace[NUM_FINGERS*6],
+            double* object_surface_discretization) 
+{
+    int code = 1;
+    while (code == 1) {
+        code = extend(*near_idx, config, epsilon_translation, epsilon_angle,
+                      steps, T, finger_workspace,
+                      object_surface_discretization);
+        if (code == 1) {
+            *near_idx = T->nodes.size() - 1;
+        }
+    }
+    return code;
+}
+
+int primitiveOne(float goToGoalRand, Tree* T, Vector3d pos_lb,
+                 Vector3d pos_ub, Vector3d goal_position,
+                 Quaterniond goal_orientation,
+                 double finger_workspace[NUM_FINGERS*6],
+                 double* object_surface_discretization)
+{
+        // bias sample toward the goal
+        Vector3d x_rand;
+        Quaterniond q_rand;
+        if (goToGoalRand < goal_biased_prob){
+            x_rand = sample_position(pos_ub, pos_lb);
+            q_rand = generate_unit_quaternion();
+        } else {
+            x_rand = goal_position;
+            q_rand = goal_orientation;
+        }
+
+        double config_rand[7] = {x_rand[0], x_rand[1], x_rand[2],
+                                 q_rand.w(), q_rand.x(), q_rand.y(),
+                                 q_rand.z()};
+        // get nearest neighbors (extend all nearest neighbor with the
+        // same object config but different finger locations)
+        std::vector<int> nears;
+        T->nearest_neighbors(config_rand, &nears);
+        
+        //extend every node_near, do: linear interpolate
+        // and check constraints
+        int numAdded = 0;
+        int code;
+        for (int i = 0; i < nears.size(); i++) {
+            // extend function
+            code = extend(nears[i], config_rand, epsilon_translation,
+                          epsilon_angle, interpolation_steps,T,
+                          finger_workspace, object_surface_discretization);
+            if (code != 0) {
+                numAdded++;
+            }
+        }
+        return numAdded;
+}
 
 static void plannerRRT(double object_position_range[6], double finger_workspace[NUM_FINGERS*6], double* object_surface_discretization, 
     double start_object_config[7], int start_finger_locations[NUM_FINGERS], double goal_object_config[7], 
@@ -105,16 +173,6 @@ static void plannerRRT(double object_position_range[6], double finger_workspace[
     *finger_path = NULL;
 	*planlength = 0;
     
-    // tunable parameters
-    double goal_thr = PI*1/180;
-    double goal_biased_prob = 0.7;
-    double primitive1_prob = 0.5;
-    double epsilon_translation = 1;
-    double epsilon_angle = PI*45/180;
-    int interpolation_steps = 10;
-    double interpolation_length = 1/double(interpolation_steps);
-    int max_samples = 1000;
-
 
     Vector3d pos_lb(object_position_range[0], object_position_range[1], object_position_range[2]);
     Vector3d pos_ub(object_position_range[3], object_position_range[4], object_position_range[5]);
@@ -138,31 +196,9 @@ static void plannerRRT(double object_position_range[6], double finger_workspace[
 
         // random sample which primitive to choose
         if (randd() < primitive1_prob){
-            //primitive 1: move the object with all fingers fixed
-            
-            // bias sample toward the goal
-            Vector3d x_rand;
-            Quaterniond q_rand;
-            if (randd() < goal_biased_prob){
-                x_rand = sample_position(pos_ub, pos_lb);
-                q_rand = generate_unit_quaternion();
-            } else {
-                x_rand = goal_position;
-                q_rand = goal_orientation;
-            }
-
-            double config_rand[7] = {x_rand[0], x_rand[1], x_rand[2], q_rand.w(), q_rand.x(), q_rand.y(), q_rand.z()};
-            //get nearest neighbors (extend all nearest neighbor with the same object config but different finger locations)
-            std::vector<int> nears;
-            T.nearest_neighbors(config_rand, &nears);
-            
-            //extend every node_near, do: linear interpolate and check constraints
-            for (int i = 0; i < nears.size(); i++) {
-
-                // extend function
-                extend(nears[i], config_rand, epsilon_translation,epsilon_angle, interpolation_steps,&T, finger_workspace, object_surface_discretization);
-
-            }
+            primitiveOne(randd(), &T, pos_lb, pos_ub, goal_position,
+                         goal_orientation, finger_workspace,
+                         object_surface_discretization);
             // if dist to goal is smaller than some threshold, goal is found
             int near_idx = T.nearest_neighbor(goal_object_config);
             double dd = dist(T.nodes[near_idx].config, goal_object_config);
@@ -201,6 +237,111 @@ static void plannerRRT(double object_position_range[6], double finger_workspace[
 
 } 
 
+static void plannerRRTCONNECT(double object_position_range[6], double finger_workspace[NUM_FINGERS*6], double* object_surface_discretization, 
+    double start_object_config[7], int start_finger_locations[NUM_FINGERS], double goal_object_config[7], 
+    double*** object_path, int*** finger_path, int* planlength, int* treesize){
+    
+    //no plan by default
+	*object_path = NULL;
+    *finger_path = NULL;
+	*planlength = 0;
+    
+
+    Vector3d pos_lb(object_position_range[0], object_position_range[1], object_position_range[2]);
+    Vector3d pos_ub(object_position_range[3], object_position_range[4], object_position_range[5]);
+
+    Vector3d start_position(start_object_config[0],start_object_config[1], start_object_config[2]);
+    Quaterniond start_orientation(start_object_config[3], start_object_config[4], start_object_config[5], start_object_config[6]);
+
+    Vector3d goal_position(goal_object_config[0],goal_object_config[1], goal_object_config[2]);
+    Quaterniond goal_orientation(goal_object_config[3], goal_object_config[4], goal_object_config[5], goal_object_config[6]);
+
+    set_rand_seed(); //set random seed by cuurent time
+    
+    // initialize the tree, create start node
+    Node start_node(start_object_config, start_finger_locations);
+    // TODO: Right now set goal for fingers to be same as start location,
+    // but we should think about how to handle this better in the future.
+    Node goal_node(goal_object_config, start_finger_locations);
+    Tree startTree, goalTree, T1, T2;
+    startTree.initial_node(&start_node); 
+    goalTree.initial_node(&goal_node);
+    bool extendStartTree = 1;
+    int numAdded, code, nearestIdx, nidx;
+    double* config;
+
+
+    //search
+    for (int kk = 0; kk < max_samples; kk++){
+        if (extendStartTree) {
+            T1 = startTree;
+            T2 = goalTree;
+        } else {
+            T1 = goalTree;
+            T2 = startTree;
+        }
+        // random sample which primitive to choose
+        if (randd() < primitive1_prob) {
+            numAdded = primitiveOne(randd(), &T1, pos_lb, pos_ub,
+                                    goal_position, goal_orientation,
+                                    finger_workspace,
+                                    object_surface_discretization);
+            // Since we may have added multiple multiple nodes, try to
+            // connect all of them.
+            if (numAdded > 0) {
+                for (nidx = T1.nodes.size() - numAdded;
+                        nidx < T1.nodes.size(); nidx++) {
+                    config = T1.nodes[nidx].config;
+                    nearestIdx = T2.nearest_neighbor(config) ;
+                    code = connect(&nearestIdx, config, epsilon_translation,
+                                   epsilon_angle, interpolation_steps, &T2, 
+                                   finger_workspace,
+                                   object_surface_discretization);
+                    if (code == 2) {
+                        // Able to connect the trees, build the plan.
+                        std::vector<int> startPath, goalPath;
+                        if (extendStartTree) {
+                            T1.backtrack(nidx, &startPath);
+                            T2.backtrack(nearestIdx, &goalPath);
+                        } else {
+                            T1.backtrack(nidx, &goalPath);
+                            T2.backtrack(nearestIdx, &startPath);
+                        }
+                        int l = startPath.size() + goalPath.size();
+                        *planlength = l;
+                        *object_path = (double**) malloc(l*sizeof(double*));
+                        *finger_path = (int**) malloc(l*sizeof(int*));
+                        for (int i = 0; i < startPath.size(); i++) {
+                            int idx = startPath.size() - i - 1;
+                            (*object_path)[i] = 
+                                startTree.nodes[startPath[idx]].config;
+                            (*finger_path)[i] =
+                                startTree.nodes[startPath[idx]].finger_locations;
+                        }
+                        for (int i = startPath.size(); i < l; i++) {
+                            int idx = i - startPath.size();
+                            (*object_path)[i] = 
+                                goalTree.nodes[goalPath[idx]].config;
+                            (*finger_path)[i] =
+                                goalTree.nodes[goalPath[idx]].finger_locations;
+                        }
+                        *treesize = startTree.nodes.size()
+                                    + goalTree.nodes.size();
+                        return;
+                    }
+                }
+            }
+            extendStartTree = ~extendStartTree;
+        } else {
+            //TODO PRIMITIVE 2
+            continue;
+        }
+    }
+    return;
+} 
+
+
+
 void mexFunction( int nlhs, mxArray *plhs[], 
 		  int nrhs, const mxArray*prhs[])
      
@@ -236,6 +377,11 @@ void mexFunction( int nlhs, mxArray *plhs[],
     {
       plannerRRT(object_range, finger_workspace, surface, start_object_config, start_finger, goal_object_config, 
         &object_path, &finger_path, &planlength, &treesize);
+    } else if (planner_id == RRTCONNECT) {
+        plannerRRTCONNECT(object_range, finger_workspace, surface,
+                          start_object_config, start_finger,
+                          goal_object_config, &object_path, &finger_path,
+                          &planlength, &treesize);
     }
     else
     {
